4721d707c3ac7d58c69f1b7c7a2b14aba3789dbf
Harper <natefneelon@gmail.com>
2023-08-31 03:03:06 -0400

    First commit: opens a window with glium and Rust

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate glium;
+
+fn main() {
+    let event_loop = glium::glutin::event_loop::EventLoop::new();
+
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(800.0, 600.0))
+        .with_title("Hello world");
+    let cb = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    event_loop.run(move | event, _, control_flow | 
+    match event {
+        glium::glutin::event::Event::WindowEvent { event, .. } => 
+        match event { 
+            glium::glutin::event::WindowEvent::CloseRequested => {
+                *control_flow = glium::glutin::event_loop::ControlFlow::Exit;
+                return;
+            },
+            _ => return,
+        },
+        _ => return,
+    });
+}
