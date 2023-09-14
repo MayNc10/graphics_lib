62bfd38df5f8433eac32e85805bbe1d7f0182bdd
Harper <natefneelon@gmail.com>
2023-09-14 01:36:42 -0400

    Refactor shader code

---
+++ src/three_d/shaders.rs
@@ -0,0 +1,124 @@
+use glium::Program;
+
+// Shader types
+#[derive(Clone, Copy)]
+pub enum ShaderType {
+    None,
+    Gouraud,
+    BlinnPhong,
+}
+
+// Create an OpenGL vextex shader for a vertex
+pub const DEFAULT_3D_SHADER: &str = r#"
+    #version 140
+
+    in vec3 position;
+    in vec3 normal;
+
+    uniform mat4 perspective;
+    uniform mat4 view;
+    uniform mat4 model;
+
+    void main() {
+        mat4 modelview = view * model;
+        gl_Position = perspective * modelview * vec4(position, 1.0);
+    }
+"#;
+
+// Create an OpenGL fragment shader for a color vertex
+// This determines the color (and other aspects) of a fragment (which is bacially just a pixel)
+pub const DEFAULT_3D_FRAG_SHADER: &str = r#"
+    #version 140
+
+    out vec4 color;
+
+    void main() {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+"#;
+
+// Create an OpenGL vextex shader for a vertex, using gouraud shading
+pub const GOURAUD_3D_SHADER: &str = r#"
+    #version 150
+
+    in vec3 position;
+    in vec3 normal;
+
+    out vec3 v_normal;
+
+    
+    uniform mat4 perspective;
+    uniform mat4 view;
+    uniform mat4 model;
+
+    void main() {
+        mat4 modelview = view * model;
+        v_normal = transpose(inverse(mat3(modelview))) * normal;
+        gl_Position = perspective * modelview * vec4(position, 1.0);
+    }
+"#;
+
+// Create an OpenGL fragment shader for a color vertex
+// This determines the color (and other aspects) of a fragment (which is bacially just a pixel
+// This uses gouraud shading
+pub const GOURAUD_3D_FRAG_SHADER: &str = r#"
+    #version 140
+
+    in vec3 v_normal;
+    out vec4 color;
+    uniform vec3 u_light;
+
+    void main() {
+        float brightness = dot(normalize(v_normal), normalize(u_light));
+        vec3 dark_color = vec3(0.6, 0.0, 0.0);
+        vec3 regular_color = vec3(1.0, 0.0, 0.0);
+        color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+    }
+"#;
+
+pub const BLINN_PHONG_3D_SHADER: &str = r#"
+    #version 150
+
+    in vec3 position;
+    in vec3 normal;
+
+    out vec3 v_normal;
+    out vec3 v_position;
+
+    
+    uniform mat4 perspective;
+    uniform mat4 view;
+    uniform mat4 model;
+
+    void main() {
+        mat4 modelview = view * model;
+        v_normal = transpose(inverse(mat3(modelview))) * normal;
+        gl_Position = perspective * modelview * vec4(position, 1.0);
+        v_position = gl_Position.xyz / gl_Position.w;
+    }
+"#;
+
+pub const BLINN_PHONG_3D_FRAG_SHADER: &str = r#"
+    #version 140
+
+    in vec3 v_normal;
+    in vec3 v_position;
+
+    out vec4 color;
+
+    uniform vec3 u_light;
+
+    const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+    const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+    const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
+    void main() {
+        float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+
+        vec3 camera_dir = normalize(-v_position);
+        vec3 half_direction = normalize(normalize(u_light) + camera_dir);
+        float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+
+        color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
+    }
+"#;
--- src/three_d/shape.rs
+++ src/three_d/shape.rs
@@ -2,6 +2,11 @@ use glium::{self, implement_vertex, Surface, Frame, uniform, uniforms::{Uniforms
 use image;
 
 use crate::matrix::*;
+use super::{shaders, animation::Animation};
+
+const FOV: f32 = std::f32::consts::PI / 3.0;
+const ZFAR: f32 = 1024.0;
+const ZNEAR: f32 = 0.1;
 
 #[derive(Copy, Clone)]
 pub struct Vertex {
@@ -16,65 +21,147 @@ pub struct Normal {
 implement_vertex!(Vertex, position);
 implement_vertex!(Normal, normal);
 
+pub struct Transform {
+    pub transform_matrix: Mat4,
+    pub rotation_matrix: Mat4,
+    pub scaling_matrix: Mat4,
+    pub translation_matrix: Mat4,
+}
+
+impl Transform {
+    pub fn set_transform_matrix(&mut self, scaling: Option<Mat4>, rotation: Option<Mat4>, translation: Option<Mat4> ) {
+        if rotation.is_some() {
+            self.rotation_matrix = rotation.unwrap();
+        }
+        if scaling.is_some() {
+            self.scaling_matrix = scaling.unwrap();
+        }
+        if translation.is_some() {
+            self.translation_matrix = translation.unwrap();
+        }
+
+        self.transform_matrix = self.scaling_matrix * self.rotation_matrix * self.translation_matrix;
+    }
+
+    pub fn set_scaling(&mut self, scaling: Mat4) {
+        self.scaling_matrix = scaling;
+
+        self.transform_matrix = self.scaling_matrix * self.rotation_matrix * self.translation_matrix;
+    }
+
+    pub fn set_rotation(&mut self, rotation: Mat4) {
+        self.rotation_matrix = rotation;
+
+        self.transform_matrix = self.scaling_matrix * self.rotation_matrix * self.translation_matrix;
+    }
+
+    pub fn set_translation(&mut self, translation: Mat4) {
+        self.translation_matrix = translation;
+
+        self.transform_matrix = self.scaling_matrix * self.rotation_matrix * self.translation_matrix;
+    }
+    
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Transform { 
+            transform_matrix: IDENTITY, 
+            rotation_matrix: IDENTITY, 
+            scaling_matrix: IDENTITY, 
+            translation_matrix: IDENTITY 
+        }
+    }
+}
+
 pub struct Shape {
     positions : VertexBuffer<Vertex>,
     normals: VertexBuffer<Normal>,
     indices: IndexBuffer<u16>,
 
-    transform_matrix: Matrix,
+    transform: Transform,
+    animation: Option<Box<dyn Animation>>,
+
+    pub shader_type: shaders::ShaderType,
+    bface_culling: glium::draw_parameters::BackfaceCullingMode,
+
 }
 
 impl Shape {
     pub fn new(positions: VertexBuffer<Vertex>, normals: VertexBuffer<Normal>, indices: IndexBuffer<u16>, 
-        transform: Option<Matrix>) -> Shape {
-        Shape { positions , normals, indices, transform_matrix: transform.unwrap_or(IDENTITY)}
+            shader_type: shaders::ShaderType, transform: Option<Transform>, animation: Option<Box<dyn Animation>>, bface_culling: bool) -> Shape {
+
+        let bface_culling = if bface_culling {
+            glium::draw_parameters::BackfaceCullingMode::CullClockwise
+        } else {
+            glium::draw_parameters::BackfaceCullingMode::CullingDisabled
+        };
+
+        Shape { positions , normals, indices, transform: transform.unwrap_or_default(), animation, shader_type, bface_culling}
     }
 }
 
 impl Shape {
-    pub fn set_transform_matrix(&mut self, mat: Matrix) {
-        self.transform_matrix = mat;
+    pub fn animate(&mut self, t: f32) {
+        if let Some(animation) = self.animation.as_mut() {
+            animation.run(t, &mut self.transform);
+        }
     }
-    pub fn get_transform(&mut self) -> &mut Matrix {
-        &mut self.transform_matrix
+    pub fn replace_animation(&mut self, animation: Box<dyn Animation>) {
+        self.animation = Some(animation);
     }
 }
 
-// Create an OpenGL vextex shader for a vertex
-const DEFAULT_3D_SHADER: &str = r#"
-    #version 140
-
-    in vec3 position;
-    in vec3 normal;
-
-    uniform mat4 matrix;
-
-    void main() {
-        gl_Position = matrix * vec4(position, 1.0);
+impl Shape {
+    pub fn set_transform_matrix(&mut self, scaling: Option<Mat4>, rotation: Option<Mat4>, translation: Option<Mat4> ) {
+        self.transform.set_transform_matrix(scaling, rotation, translation);
     }
-"#;
 
-// Create an OpenGL fragment shader for a color vertex
-// This determines the color (and other aspects) of a fragment (which is bacially just a pixel)
-const DEFAULT_3D_FRAG_SHADER: &str = r#"
-    #version 140
-
-    out vec4 color;
-
-    void main() {
-        color = vec4(1.0, 0.0, 0.0, 1.0);
+    pub fn set_scaling(&mut self, scaling: Mat4) {
+        self.transform.set_scaling(scaling)
     }
-"#;
+
+    pub fn set_rotation(&mut self, rotation: Mat4) {
+        self.transform.set_rotation(rotation)
+    }
+
+    pub fn set_translation(&mut self, translation: Mat4) {
+        self.transform.set_translation(translation)
+    }
+}
+
 
 impl Shape {
-    pub fn draw(&self, frame: &mut Frame, display: &Display) {
-        let program = glium::Program::from_source(
-            display, DEFAULT_3D_SHADER, 
-            DEFAULT_3D_FRAG_SHADER, None).unwrap(); 
+    pub fn draw(&self, frame: &mut Frame, light: &[f32; 3], view: &Mat4, program: &glium::Program) {
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess, // When should we use pixel values? IfLess (than the value already there)
+                write: true, // Write the new pixel depths to the depth buffer
+                .. Default::default()
+            },
+            backface_culling: self.bface_culling,
+            .. Default::default()
+        };
 
-        let uniforms = uniform! {matrix: self.transform_matrix};
+        // perspective matrix        
+        let perspective = {
+            let (width, height) = frame.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
 
-        frame.draw((&self.positions , &self.normals), &self.indices, &program, &uniforms,
-            &Default::default()).unwrap();
+            let f = 1.0 / (FOV / 2.0).tan();
+
+            [
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (ZFAR+ZNEAR)/(ZFAR-ZNEAR)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*ZFAR*ZNEAR)/(ZFAR-ZNEAR),   0.0],
+            ]
+        };
+
+        let uniforms = uniform! {
+                model: self.transform.transform_matrix.inner, view: view.inner, perspective: perspective, u_light: *light};
+
+        frame.draw((&self.positions , &self.normals), &self.indices, program, &uniforms,
+            &params).unwrap();
     }
 }
